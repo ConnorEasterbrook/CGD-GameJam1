c97c6729c15505c2a32df110c3295fc276979332
Joshua <joshua2.barrett@live.uwe.ac.uk>
2022-10-10 21:40:32 +0100

    Random Building spawner

--- Assets/Scripts/SpawnPlats/SpawnBuildings.cs
+++ Assets/Scripts/SpawnPlats/SpawnBuildings.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SpawnBuildings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject building;
+    public float spawnTime;
+    public Transform spawnLocation;
+    public float spawntimecooldown = 0f;
+    Transform randPosition;
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        
+        spawntimecooldown += Time.deltaTime;
+
+        if (spawntimecooldown >= spawnTime)
+        {
+            spawnLocation.position = new Vector3(spawnLocation.position.x, spawnLocation.position.y + Random.Range(-5.0f, 5f), spawnLocation.position.z);
+            Instantiate(building, spawnLocation.position, building.transform.rotation);
+            spawntimecooldown = 0f;
+        }
     }
 }
