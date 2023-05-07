2ee59e56928ac529454e429e6287b6e222fa8339
Tom Pasley <thomas2.pasley@live.uwe.ac.uk>
2023-05-07 18:00:40 +0100

    Animation changes + graphical tweaks + nice sound changes

--- Assets/Scripts/Pickups/Score_Pickup.cs
+++ Assets/Scripts/Pickups/Score_Pickup.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Score_Pickup : MonoBehaviour
 {
     public int scoreFromPickup = 20;
+    private bool canGivePoints = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Player" && canGivePoints)
         {
             ScoreOverTime.score += scoreFromPickup;
-            Destroy(gameObject);
+            GetComponent<AudioSource>().Play();
+            GetComponent<SpriteRenderer>().enabled=false;
+            canGivePoints=false;
+            Destroy(gameObject,5f);
         }
     }
 }
